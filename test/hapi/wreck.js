3524aa54030e16b1d84f2ac497495ba97a9913c7
thsmale <tsmale@gmail.com>
2024-01-02 23:33:36 -0800

    rm code starting addy server from javascript
    results in infinite loop

--- test/hapi/wreck.js
+++ test/hapi/wreck.js
@@ -1,57 +1,11 @@
 const Wreck = require('@hapi/wreck');
 const Lab = require('@hapi/lab');
 const Code = require('@hapi/code');
-const { spawn } = require('node:child_process')
-const { chdir, kill } = require('node:process');
 
 const { expect } = Code;
 const lab = exports.lab = Lab.script();
 
 lab.experiment('server can receive and respond to HTTP requests', () => {
-	let pid; 
-	let output;
-	let error;
-	let server;
-	lab.before(async () => {
-		// run the server
-		// in the future you should write a test hapi server here as well
-		/*
-		chdir('../../');
-		return new Promise((resolve, reject) => {
-			server = spawn('./server', [], { detached: true });
-			server.unref();
-			server.stdout.on('data', function(data) {
-				console.log(data.toString()); 
-				return resolve('server started');
-			});
-			server.stderr.on('data', (data) => {
-				console.error(`stderr: ${data}`);
-				return reject('server failed to start');
-			});
-		});
-		*/
-		
-		/*
-		if (error) {
-			console.error(error);
-			throw Error('Error starting server')
-		}
-		console.log(output.toString());
-		*/
-	})
-
-	lab.after(() => {
-		// kill the process
-		/*
-		server.kill('SIGINT');
-		server.on('close', (code) => {
-			if (code !== 0) {
-			  console.log(`grep process exited with code ${code}`);
-			}
-		}); 
-		*/
-	})
-
     lab.test('send a single GET request', async () => {
 		const { res, payload } = await Wreck.get('http://localhost:3000');
 		expect(res.statusCode).to.equal(200);
