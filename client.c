5cd77181edff2e16e306ee2eb463e331492c03d5
thsmale <tsmale@gmail.com>
2024-01-03 00:23:59 -0800

    resolve issue #17

--- client.c
+++ client.c
@@ -4,6 +4,7 @@ int main() {
 	struct Http options;
 	char *host = getenv("ADDY_HOST");
 	char *port = getenv("PORT");
+	char headers[SMALL];
 	if (!host) {
 		host = "localhost";
 	}
@@ -11,12 +12,14 @@ int main() {
 		// set port to default 3000
 		port = "3000";
 	}
+    int ret = snprintf(headers, sizeof(char) * SMALL, "host: %s:%s", host, port);
+    handle_snprintf(ret, sizeof(char) * SMALL);
 	options.method = "GET";
 	options.route = "/";
 	options.version = "HTTP/1.1";
 	options.host = host;
 	options.port = port;
-	options.headers = "host: localhost:3000";
+	options.headers = headers;
 	options.payload = "";
 
 	char response[MEDIUM];
